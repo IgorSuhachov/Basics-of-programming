98b4a72fedfc9ce0c16501a22caf8e7dd8f7a714
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-03-17 13:12:33 +0300

    Create Beginner.cpp

---
+++ Project/Beginner.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+void Beginner() {
+	setlocale(LC_ALL, "rus");
+
+	int task_number_Beginner;
+
+	cout << "Выберите номер задания." << endl;
+	cin >> task_number_Beginner;
+
+	switch (task_number_Beginner) {
+	case 1:
+		break;
+
+	case 2:
+		break;
+
+	case 3:
+		break;
+
+	case 4:
+		break;
+
+	case 5:
+		break;
+
+	case 6:
+		break;
+
+	case 7:
+		break;
+
+	case 8:
+		break;
+
+	case 9:
+		break;
+
+	case 10:
+		break;
+
+	default:
+		break;
+	}
+}
