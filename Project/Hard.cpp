bc4d94bd4787c15d60407e6a628713357cda4b9a
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-03-17 13:49:27 +0300

    Create Hard.cpp

---
+++ Project/Hard.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+void Hard() {
+	setlocale(LC_ALL, "rus");
+
+	int task_number_Hard;
+
+	cout << "Выберите номер задания." << endl;
+	cin >> task_number_Hard;
+
+	switch (task_number_Hard) {
+	case 1:
+		break;
+
+	case 2:
+		break;
+
+	case 3:
+		break;
+
+	case 4:
+		break;
+
+	case 5:
+		break;
+
+	case 6:
+		break;
+
+	case 7:
+		break;
+
+	case 8:
+		break;
+
+	case 9:
+		break;
+
+	case 10:
+		break;
+
+	default:
+		break;
+	}
+}
