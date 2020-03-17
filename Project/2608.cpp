9b986d92286b1490373c7d5fa4937a4d35821bfd
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-03-17 15:08:56 +0300

    up

--- Project/2608.cpp
+++ Project/2608.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 void task2608() {
+	int n, n_1;
+	cout << "Сравнение чисел используя оператор ветвления" << endl;
+	
+	cout << "Введите 1 число: ";
+	cin >> n;
+
+	cout << "Введите 2 число: ";
+	cin >> n_1;
+
+	if (n<n_1)
+		cout << "Меньше" << endl;
+
+	else if (n>n_1)
+		cout << "Больше" << endl;
+
+	else
+		cout << "Одинаково" << endl;
 
 	system("pause");
 	system("cls");
