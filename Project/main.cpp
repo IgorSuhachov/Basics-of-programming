5b1d7fb78b2dd115476a701096553f6d805abae0
IgorSuhachov <42006386+IgorSuhachov@users.noreply.github.com>
2020-03-17 13:17:49 +0300

    Create main.cpp

---
+++ Project/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	void Normal(), Beginner(), Hard();
+
+	int task_number_Beginner;
+	int task_number_Hard;
+	int level;
+
+	while (true) {
+		cout << "Выберите желаемый уровень сложности задач:";
+		cin >> level;
+
+		switch (level) {
+		case 1:
+			Beginner();
+			break;
+
+		case 2:
+			Normal();
+			break;
+
+		case 3:
+			Hard();
+			break;
+
+		default:
+			cout << "Вы ввели невеный уровень сложности.";
+			break;
+		}
+	}
+
+	return 0;
+}
